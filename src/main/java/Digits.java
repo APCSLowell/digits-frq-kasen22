a1e73930f4d5dc3496c9893ad760eca79e199405
kasen22 <143123847+kasen22@users.noreply.github.com>
2024-04-15 23:53:22 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,28 +7,30 @@ public class Digits
 
 	public Digits(int num)
 	{ 
-	digitList = new ArrayList <Integer>();
-		if (num == 0){
+		digitList = new ArrayList <Integer>();
+		if(num == 0){
 			digitList.add(0);
 		}
-	int n = num;
-	while (n>0){
-		digitList.add(0,n%10);
-		n = n/10;
-	}
+		int n = num;
+		while(num > 0 ){
+			digitList.add(0, n%10);
+			n = n%10;
+		}
+	    
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ 
-	for (int i = 0; i < digitList.size()-1; i++){
-		if (digitList.get(i) <= digitList.get(i+1)){
-			return true;
+		for(int i = 1; i < digitList.size(); i++){
+			if(digitList.get(i) > digitList.get(i-1)){
+				return true;
+		// for(int i = 0; i < didgitList.size()-1; i++){
+		// 	if(digitList.get(i+1) <= digitList.get(i)){
+		// 		return false;
+			}
 		}
-	}
 		return false;
-
 	}
-	
 	public String toString()
 	{
 		return digitList.toString();
